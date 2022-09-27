f0d56620040c79b40955ea00f546a1cc85fe0114
milaguileras <milaguileras@gmail.com>
2022-09-26 20:33:26 -0500

    changed image paths on data

--- travelProject/src/app/landing-page/landing-page.component.ts
+++ travelProject/src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Destination } from '../models/destination.model';
+import { Group } from '../models/group.model';
+import { DestinationsService } from '../services/destination.service';
+import { GroupService } from '../services/group.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,12 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
+  destinations!: Destination[]
+  groups!: Group[]
 
+  constructor(private router: Router, private destinationService: DestinationsService, 
+    private groupService: GroupService) {
+  }
 
-
-  constructor() { }
 
   ngOnInit(): void {
+    this.destinationService.getDestinations().subscribe((destinations) => {
+      this.destinations = destinations
+    })
+    this.groupService.getGroups().subscribe((groups) => {
+      this.groups = groups
+    })
   }
 
 }
