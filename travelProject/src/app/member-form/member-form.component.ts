dd0431cf2d5133ebda4d2111b3e46b049265c664
milaguileras <milaguileras@gmail.com>
2022-09-29 18:21:23 -0500

    final product

--- travelProject/src/app/member-form/member-form.component.ts
+++ travelProject/src/app/member-form/member-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DestinationsService } from '../services/destination.service';
@@ -7,24 +7,33 @@ import { GroupService } from '../services/group.service';
 import { Group } from '../models/group.model';
 import { Member } from '../models/member.model';
 import { MemberService } from '../services/member.service';
+import { group } from '@angular/animations';
 
 @Component({
-  selector: 'app-user-form',
+  selector: 'app-member-form',
   templateUrl: './member-form.component.html',
-  styleUrls: ['./member-form.component.scss']
+  styleUrls: ['./member-form.component.scss'],
 })
-
 export class MemberFormComponent implements OnInit {
   memberForm!: FormGroup;
   submit!: boolean;
   destinations!: Destination[];
   groups!: Group[];
+  groupsFiltered!: Group[];
   errorMessage!: string;
-  newMember: Member = new Member()
+  newMember: Member = new Member();
+  //kr
+  selectedGroup!: number;
+  @ViewChild('groupChoice') groupChoice!: ElementRef;
 
-
-  constructor(private form: FormBuilder, private router: Router, private destinationService: DestinationsService, 
-    private groupService: GroupService, private memberService: MemberService) {
+  constructor(
+    private form: FormBuilder,
+    private router: Router,
+    private destinationService: DestinationsService,
+    private groupService: GroupService,
+    private memberService: MemberService
+  ) 
+  {
     this.memberForm = form.group({
       name: [null, [Validators.required]],
       email: [
@@ -33,46 +42,66 @@ export class MemberFormComponent implements OnInit {
       ],
       phone: [null, [Validators.required]],
       destination: [null, [Validators.required]],
-      group: [null, [Validators.required]]
+      group: [null, [Validators.required]],
     });
-  }
+  };
 
   ngOnInit(): void {
     this.memberForm.valueChanges.subscribe((value) => {
       this.newMember.MemberEmail = value.email;
       this.newMember.MemberName = value.name;
       this.newMember.MemberPhone = value.phone;
+      // if (value.destination)
+      console.log(value)
+    });
+
+    this.memberForm.get('destination')?.valueChanges.subscribe((value)=> {
+      if(value){
+        this.groupsFiltered = this.groups.filter((group)=>{
+          return group.OrganizationName === value
+        })
+      }
     });
 
     this.destinationService.getDestinations().subscribe((destinations) => {
-      this.destinations = destinations.filter((destination)=>{
-        return destination.OrganizationName !== 'Coming Soon'
+      this.destinations = destinations.filter((destination) => {
+        return destination.OrganizationName !== 'Coming Soon';
       });
-    })
+    });
 
     this.groupService.getGroups().subscribe((groups) => {
-      this.groups = groups
-    })
-  }
+      this.groups = groups;
+      console.log(groups);
+    });
+  };
 
   canDeactivate(): boolean {
     console.log(!this.memberForm.touched);
     return !this.memberForm.touched || this.submit;
-  }
+  };
 
-  insertMember(member: Member, id: number ): void {
-    // this.memberService.addMembers(member, id).subscribe({
-    //   next: (member) => {
-    //     window.location.reload();
-    //   },
-    // });
-  }
+  insertMember(groupId: number, member: Member): void {
+    console.log(groupId);
+    console.log(member);
+    console.log(this.newMember);
+    this.memberService.addMembertoGroup(groupId, this.newMember).subscribe({
+      next: (newMember) => {
+        console.log(groupId);
+        window.location.reload();
+      },
+    });
+  };
 
-  onSubmit(): void {
-    if (this.memberForm.invalid) {
-      console.log('submitMember(): this.memberForm.invalid = true ');
-      return;
-    }
-    this.insertMember(this.newMember, this.newMember.MemberId);
-  }
+  getSelectedGroup(): void {
+    this.selectedGroup = this.groupChoice.nativeElement.value;
+    //console.log(this.selectedGroup);
+  };
+
+  onSubmit(formValues: any): void {
+    this.selectedGroup = this.groupChoice.nativeElement.value;
+    console.log(this.groupChoice.nativeElement);
+    const groupID = this.selectedGroup;
+    //console.log(this.selectedGroup);
+    this.insertMember(groupID, formValues);
+  };
 }
--- travelProject/src/app/services/member.service.ts
+++ travelProject/src/app/services/member.service.ts
@@ -4,28 +4,28 @@ import { Observable } from 'rxjs';
 import { Member } from '../models/member.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class MemberService {
-    groupUrl: string = 'http://localhost:8082/api/groups'
-    jsonContentTypeHeaders = {
-        headers: new HttpHeaders().set('Content-Type', 'application/json'),
-    };
+  groupUrl: string = 'http://localhost:8082/api/groups';
+  jsonContentTypeHeaders = {
+    headers: new HttpHeaders().set('Content-Type', 'application/json'),
+  };
 
-    constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) {}
 
-    getMembers():Observable<Member[]>{
-        const data: Observable<Member[]> = this.http.get<Member[]>(this.groupUrl);
-        return data
-    }
+  getMembers(): Observable<Member[]> {
+    const data: Observable<Member[]> = this.http.get<Member[]>(this.groupUrl);
+    return data;
+  }
 
-    addMembers(groupId: number, member: Member): Observable<Member> {
-        const results: Observable<Member> = this.http.post<Member>(
-            `${this.groupUrl}/${groupId}/members`,
-            member,
-            this.jsonContentTypeHeaders);
-        console.log(`addMemberToGroup() returned ${results}`);
-        return results;
-    }
+  addMembertoGroup(groupId: number, member: Member): Observable<Member> {
+    const results: Observable<Member> = this.http.post<Member>(
+      `${this.groupUrl}/${groupId}/members`,
+      member,
+      this.jsonContentTypeHeaders
+    );
+    console.log(`addMemberToGroup() returned ${results}`);
+    return results;
+  }
 }
-
